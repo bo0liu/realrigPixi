130cb0e1c0c1932a798c91921efbd802af95926a
bo0liu <duke-lsb@163.com>
2016-05-11 16:49:07 +0800

    Initial commit

---
+++ typings/tsd.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="uuid/UUID.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="bootstrap/bootstrap.d.ts" />
+/// <reference path="pixi.js/pixi.js.d.ts" />
